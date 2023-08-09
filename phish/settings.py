c954e67d80a8f23176663001cdeb23f4bc26e30f
Haramiday <45460677+Haramiday@users.noreply.github.com>
2023-08-09 11:36:13 +0100

    Update settings.py

--- phish/settings.py
+++ phish/settings.py
@@ -45,9 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
-    'corsheaders'
+    
 ]
 
 MIDDLEWARE = [
